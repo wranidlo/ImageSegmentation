196f1477fa9f82cc96286cf4a8942a44f220c3e0
Aoxter <pkopycki@onet.pl>
2020-05-07 15:43:12 +0200

    Evaluation - support for series of data, new algorithms
    Added list to store many images to evaluate.  Interface fixes. New algorithms of evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,98 +1,150 @@
 import numpy as np
-from sklearn.metrics import jaccard_similarity_score, jaccard_score, f1_score, recall_score, mean_absolute_error
+from sklearn.metrics import jaccard_similarity_score, jaccard_score, f1_score, recall_score, mean_squared_error, explained_variance_score
 import cv2
 from matplotlib import pyplot as plt
 
 
 def printMainMenu():
-    print("MENU")
-    print("1. load true segmentation")
-    print("2. load predicted segmentation")
-    print("3. display current evalutaion results")
-    print("4. evaluate loaded segmentation with all algorithms")
-    print("5. choose algorithm to evaluate loaded segmentation")
+    print("\nMENU")
+    print("1. load images to evaluation")
+    print("2. delete images from segmentation")
+    print("3. display loaded images")
+    print("4. display current evalutaion results")
+    print("5. evaluate loaded segmentation with all algorithms")
+    print("6. display algorithms of evaluation")
     if preconfiguredPaths == 0:
-        print("6. toggle on preconfigured paths (TESTING)")
+        print("7. toggle on preconfigured paths (TESTING)")
     else:
-        print("6. toggle off preconfigured paths (TESTING)")
+        print("7. toggle off preconfigured paths (TESTING)")
     print("0. exit")
 
 
 def printAlgorithmsMenu():
+    # classification
     print("1. Jaccarda Score")
     print("2. F1 Score")
-    print("3. Recall Score (additional)")
-    print("4. Mean absolute error (additional)")
+    # regression
+    print("3. Explained Varince Score (additional)")
+    print("4. Mean Squared Error")
     print("0. go back")
 
 
 class Evaluation:
     def __init__(self):
-        self.img_true = None
-        self.img_pred = None
-        self.js_none = None
-        self.js = {}
-        self.f1_none = None
-        self.f1 = {}
-        # additional algorithms
-        self.rc = {}
-        self.mae = None
+        self.trueSeg = []
+        self.predSeg = []
+        self.trueSegPaths = []
+        self.predSegPaths = []
+        self.js_none = []
+        self.js = []
+        self.f1_none = []
+        self.f1 = []
+        self.evs = []
+        self.mse = []
+
+    def loadImagesSet(self, pathPred, pathTrue):
+        self.loadTrueImage(pathTrue)
+        self.loadPredictedImage(pathPred)
+        self.trueSegPaths.append(pathTrue)
+        self.predSegPaths.append(pathPred)
+
+    def deleteImageSet(self, id):
+        #TODO exception for bad ID
+        id = int(id)
+        del self.trueSeg[id]
+        del self.predSeg[id]
+        del self.trueSegPaths[id]
+        del self.predSegPaths[id]
+        if id > len(self.js_none):
+            del self.js_none[id]
+            del self.js[id]
+            del self.f1_none[id]
+            del self.f1[id]
+            del self.evs[id]
+            del self.mse[id]
+
+    def displayImages(self):
+        id = 0;
+        for path in self.trueSegPaths:
+            print("--------------------------------------------------")
+            print("ID:", id)
+            print("true: " + path)
+            print("pred: " + self.predSegPaths[id])
+            id += 1
+        print("--------------------------------------------------")
 
     def loadImageAsArray(self, path):
         image = cv2.imread(path)
         return np.array(image).ravel()
 
-    def setTrueImage(self, path):
-        self.img_true = self.loadImageAsArray(path)
+    def loadTrueImage(self, path):
+        self.trueSeg.append(self.loadImageAsArray(path))
 
-    def setPredictedImage(self, path):
-        self.img_pred = self.loadImageAsArray(path)
+    def loadPredictedImage(self, path):
+        self.predSeg.append(self.loadImageAsArray(path))
 
     # Jaccard Index also known as Intersection-Over-Union (IoU)
     def jaccardScoreAlgorithm(self):
         # iou = jaccard_similarity_score(self.img_true, self.img_pred) - old method
-        self.js_none = jaccard_score(self.img_true, self.img_pred, average=None)
-        self.js['micro'] = jaccard_score(self.img_true, self.img_pred, average="micro")
-        self.js['macro'] = jaccard_score(self.img_true, self.img_pred, average="macro")
-        self.js['weighted'] = jaccard_score(self.img_true, self.img_pred, average="weighted")
+        id = 0
+        for pred in self.predSeg:
+            self.js_none.append(jaccard_score(self.trueSeg[id], pred, average=None))
+            self.js.append({})
+            self.js[id]['micro'] = jaccard_score(self.trueSeg[id], pred, average="micro")
+            self.js[id]['macro'] = jaccard_score(self.trueSeg[id], pred, average="macro")
+            self.js[id]['weighted'] = jaccard_score(self.trueSeg[id], pred, average="weighted")
+            id += 1
 
     # F1 Score also known as Dice Coefficient
     def F1ScoreAlgorithm(self):
-        self.f1_none = f1_score(self.img_true, self.img_pred, average=None)
-        self.f1['micro'] = f1_score(self.img_true, self.img_pred, average="micro")
-        self.f1['macro'] = f1_score(self.img_true, self.img_pred, average="macro")
-        self.f1['weighted'] = f1_score(self.img_true, self.img_pred, average="weighted")
+        id = 0
+        for pred in self.predSeg:
+            self.f1_none.append(f1_score(self.trueSeg[id], pred, average=None))
+            self.f1.append({})
+            self.f1[id]['micro'] = f1_score(self.trueSeg[id], pred, average="micro")
+            self.f1[id]['macro'] = f1_score(self.trueSeg[id], pred, average="macro")
+            self.f1[id]['weighted'] = f1_score(self.trueSeg[id], pred, average="weighted")
+            id += 1
 
     # Recall Score
-    def recallScoreAlgorithm(self):
-        self.rc['weighted'] = recall_score(self.img_true, self.img_pred, average="weighted")
+    def explainedVarianceScoreAlgorithm(self):
+        id = 0
+        for pred in self.predSeg:
+            self.evs.append(explained_variance_score(self.trueSeg[id], pred))
+            id += 1
 
     # Mean absolute error
-    def meanAbsoluteErrorAlgorithm(self):
-        self.mae = mean_absolute_error(self.img_true, self.img_pred)
+    def meanSquaredErrorAlgorithm(self):
+        id = 0
+        for pred in self.predSeg:
+            self.mse.append(mean_squared_error(self.trueSeg[id], pred))
+            id += 1
 
     # Call all evaluation algorithms
     def allAlgorithms(self):
         self.jaccardScoreAlgorithm()
         self.F1ScoreAlgorithm()
-        self.recallScoreAlgorithm()
-        self.meanAbsoluteErrorAlgorithm()
+        self.explainedVarianceScoreAlgorithm()
+        self.meanSquaredErrorAlgorithm()
 
     def displayResults(self):
         print("Results:")
-        print("\nJaccarda Index (average type: result [0-1] )")
-        print("None: ", self.js_none)
-        for k in self.js:
-            print(k, ": ", self.js[k])
-        print("\nF1 Score (average type: result [0-1] )")
-        print("None: ", self.f1_none)
-        for k in self.f1:
-            print(k, ": ", self.f1[k])
-        print("\nadditional algorithms")
-        print("\nRecall Score (average type: result [0-1] )")
-        for k in self.rc:
-            print(k, ": ", self.rc[k])
-        print("\nMean absolute error: ", self.mae)
+        id = 0
+        for _ in self.js:
+            print("--------------------------------------------------")
+            print("Evaluation for images", id)
+            print("\nJaccarda Index (average type: result [0-1] )")
+            print("None: ", self.js_none[id])
+            for k in self.js[id]:
+                print(k, ": ", self.js[id][k])
+            print("\nF1 Score (average type: result [0-1] )")
+            print("None: ", self.f1_none[id])
+            for k in self.f1[id]:
+                print(k, ": ", self.f1[id][k])
+            print("\nExplained Variance Score", self.evs[id])
+            print("\nMean squared error: ", self.mse[id])
+            id += 1
+        print("--------------------------------------------------")
 
 
 if __name__ == "__main__":
@@ -113,33 +165,42 @@ if __name__ == "__main__":
             break
         elif option == 1:
             if preconfiguredPaths == 0:
-                path = input("Give path to image:")
-                evaluation.setTrueImage(path)
+                pathTrue = input("Give path to ground truth image:")
+                pathPred = input("Give path to predicted image:")
+                evaluation.loadImagesSet(pathPred, pathTrue)
+                print("\nimages loaded from:\n" + pathTrue + "\n\tand\n" + pathPred)
             else:
-                evaluation.setTrueImage(pathTrueSeg)
+                evaluation.loadImagesSet(pathPredSeg, pathTrueSeg)
+                print("\nimages loaded from:\n" + pathTrueSeg + "\n\tand\n" + pathPredSeg)
         elif option == 2:
-            if preconfiguredPaths == 0:
-                path = input("Give path to image:")
-                evaluation.setPredictedImage(path)
-            else:
-                evaluation.setPredictedImage(pathPredSeg)
+            id = input("Give id images to delete")
+            evaluation.deleteImageSet(id)
         elif option == 3:
-            evaluation.displayResults()
+            evaluation.displayImages()
         elif option == 4:
-            evaluation.allAlgorithms()
+            evaluation.displayResults()
         elif option == 5:
+            evaluation.allAlgorithms()
+            print("\nevaluation ended successfully")
+        elif option == 6:
             printAlgorithmsMenu()
             option_2 = int(input())
-            if option_2 == 1:
-                evaluation.jaccardScoreAlgorithm()
-            elif option_2 == 2:
-                evaluation.F1ScoreAlgorithm()
-            elif option_2 == 3:
-                evaluation.recallScoreAlgorithm()
-            elif option_2 == 4:
-                evaluation.meanAbsoluteErrorAlgorithm()
-        elif option == 6:
+            # if option_2 == 1:
+            #     evaluation.jaccardScoreAlgorithm()
+            #     print("\nevaluation ended successfully")
+            # elif option_2 == 2:
+            #     evaluation.F1ScoreAlgorithm()
+            #     print("\nevaluation ended successfully")
+            # elif option_2 == 3:
+            #     evaluation.explainedVarianceScoreAlgorithm()
+            #     print("\nevaluation ended successfully")
+            # elif option_2 == 4:
+            #     evaluation.meanSquaredErrorAlgorithm()
+            #     print("\nevaluation ended successfully")
+        elif option == 7:
             if preconfiguredPaths == 0:
                 preconfiguredPaths = 1
+                print("\npreconfigured paths: on")
             else:
                 preconfiguredPaths = 0
+                print("\npreconfigured paths: off")
